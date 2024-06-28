f73a8f1b40625daca9804e49569480cce0896822
Jannik <72359748+jannikm00@users.noreply.github.com>
2024-06-28 14:06:32 +0200

    Update slavetest.ino

--- Gaming Table Arduino/Programmierung/Gaming_Table/Gaming_Table_Nunchuck/slavetest.ino
+++ Gaming Table Arduino/Programmierung/Gaming_Table/Gaming_Table_Nunchuck/slavetest.ino
@@ -1,6 +1,9 @@
 #include <pitches.h>
 #include "MusikVariablen.h"
 #include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+int MAXLED = 100;
+int LED_PIN = 6;
 int selct;
 int melodyPin = 3;
 int control_music = 0;
@@ -15,15 +18,22 @@ float pBN_pinkpanther = 8;
 float pBN_simpson = 8;
 float pBN_tokyodrift = 8;
 int change_music_in_home_screen_status = 2;
+Adafruit_NeoPixel led(MAXLED, 6, NEO_GRB + NEO_KHZ800);
 void setup() {
-
+  led.begin();
   Wire.begin(9);
-  Wire.onReceive(receiveEvent);
+
   Serial.begin(9600);
+  for (int i = 0; i < MAXLED; i++) {
+    led.setPixelColor(i, led.Color(0, 0, 0));
+  }
+  led.show();
 }
 void loop() {
+  Wire.onReceive(receiveEvent);
   select_music();
   delay(10);
+  Serial.println(selct);
 }
 
 // function that executes whenever data is received from master
@@ -45,30 +55,34 @@ void select_music() {
   //Title Musik
   if (selct == 0) {
     mario_theme();
+    LED_Breathe_Red();
   }
-
-  //Race Game Musik
+  Race Game Musik
   if (selct == 1) {
-    star_wars_theme();
+    LED_Shoot_White();
+    Tokyo_Drift_theme();
   }
   //Tic Tac Toe Musik
   if (selct == 2) {
-    mario_theme();
-  }
-  //Collect Coin Musik
-  if (selct == 3 ) {
-    pacman_theme();
-  }
-  //Jump and Run Musik
-  if (selct == 4 ) {
-    pacman_theme();
-  }
-  //Stop and Go Musik
-  if (selct == 5 ) {
     underworld_theme();
   }
+  //Collect Coin Musik
+  if (selct == 3) {
+    pacman_theme();
+    LED_Gold_Glitter();
+  }
+  /*
+  //Jump and Run Musik
+  if (selct == 4) {
+    pacman_theme();
+  }*/
+  //Stop and Go Musik
+  if (selct == 5) {
+    underworld_theme();
+    LED_Red_Flash();
+  }
   //platzhalter für andere Spiele
-  
+
   if (selct == 6 || selct == 0 && change_music_in_home_screen_status == 6) {
     Tokyo_Drift_theme();
   }
@@ -79,8 +93,11 @@ void select_music() {
   if (selct == 9 || selct == 0 && change_music_in_home_screen_status == 9) {
   }
   //Easter Egg Musik
-  if (selct == "easter_egg") {
-    pacman_theme();
+  if (selct == 103) {  //"easter egg"
+    pink_panther_theme();
+  }
+  if (selct == 232) {
+    delay(10);
   }
 }
 
@@ -249,5 +266,58 @@ void Tokyo_Drift_theme() {
   }
   if (thisNote >= size) {
     thisNote = 0;
+  }
+}
+
+void LED_Breathe_Red() {
+  for (int i = 0; i < MAXLED; i++) {
+    led.setPixelColor(i, led.Color(255, 0, 0));
+  }
+  led.show();
+  for (int i = 50; i < 200; i++) {
+    led.setBrightness(i);
+    led.show();
+    delay(1);
+  }
+  for (int i = 200; i > 50; i--) {
+    led.setBrightness(i);
+    led.show();
+    delay(1);
+  }
+}
+
+void LED_Shoot_White() {
+  for (int i = 0; i < MAXLED; i++) {
+    led.setPixelColor(i, led.Color(255, 255, 255));
+    led.show();
+    led.setPixelColor(i - 10, led.Color(0, 0, 0));
+    led.show();
+  }
+}
+
+void LED_Gold_Glitter() {
+  for (int i = 0; i < MAXLED; i++) {
+    int randompixel = random(0, 500);
+    led.setPixelColor(randompixel, led.Color(255, 255, 0));
+    led.show();
+  }
+  for (int i = 0; i < MAXLED; i++) {
+    int randompixel = random(0, 100);
+    led.setPixelColor(randompixel, led.Color(0, 0, 0));
+  }
+  led.show();
+}
+void LED_Red_Flash() {
+  if (random(0, 100) == 42) {
+    for (int i = 0; i < MAXLED; i++) {
+      led.setPixelColor(i, led.Color(255, 0, 0));
+    }
+    led.show();
+  }
+  if (random(0, 10) == 1) {
+    for (int i = 0; i < MAXLED; i++) {
+      led.setPixelColor(i, led.Color(0, 0, 0));
+    }
+    led.show();
   }
 }
